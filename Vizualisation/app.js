3e1d244e5a57bf934853f216377df791e2fb2c9b
Djalil MOUSSA <mehdi-djalil.moussa-boudjemaa@etu.univ-cotedazur.fr>
2023-01-02 23:00:11 +0100

    Update server

--- Vizualisation/app.js
+++ Vizualisation/app.js
@@ -4,107 +4,62 @@ const app = express();
 const port = 3000;
 var JDBC = require("jdbc");
 var jinst = require("jdbc/lib/jinst");
-var asyncjs = require("async");
-var util = require("util");
+var fs = require("fs");
+var cors = require("cors");
+app.use(cors());
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.get("/", (req, res) => {
-    var data = [];
-    //create a jvm and specify the jars required in the classpath and other jvm parameters
-    if (!jinst.isJvmCreated()) {
-        jinst.addOption("-Xrs");
-        jinst.setupClasspath([
-            "./lib/hive-jdbc-3.1.3-standalone.jar",
-            "./lib/hadoop-common-3.3.4.jar",
-        ]);
+/// uncomment if you wanna use hive connection
+if (!jinst.isJvmCreated()) {
+    jinst.addOption("-Xrs");
+    jinst.setupClasspath([
+        "./lib/hive-jdbc-3.1.3-standalone.jar",
+        "./lib/hadoop-common-3.3.4.jar",
+    ]);
+}
+var conf = {
+    url: "jdbc:hive2://localhost:10000",
+    drivername: "org.apache.hive.jdbc.HiveDriver",
+    properties: {},
+};
+var hive = new JDBC(conf);
+hive.initialize(function (err) {
+    if (err) {
+        console.log(err);
     }
+});
+///
 
-    //read the input arguments
-
-    //specify the hive connection parameters
-
-    var conf = {
-        url: "jdbc:hive2://localhost:10000",
-        drivername: "org.apache.hive.jdbc.HiveDriver",
-        properties: {},
-    };
-
-    var hive = new JDBC(conf);
-
-    //initialize the connection
-
-    hive.initialize(function (err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-
-    // create the connection
-
+app.get("/hive", (req, res) => {
+    var data = [];
     hive.reserve(function (err, connObj) {
         if (connObj) {
             console.log("Connection : " + connObj.uuid);
             var conn = connObj.conn;
-
-            asyncjs.series([
-                //set hive paramters if required. A sample property is set below
-                function (callback) {
-                    conn.createStatement(function (err, statement) {
-                        if (err) {
-                            callback(err);
-                        } else {
-                            statement.execute(
-                                "SET hive.metastore.sasl.enabled=false",
-                                function (err, resultset) {
-                                    if (err) {
-                                        callback(err);
-                                    } else {
-                                        console.log(
-                                            "Seccessfully set the properties"
-                                        );
-                                        callback(null, resultset);
-                                    }
-                                }
-                            );
-                        }
-                    });
-                },
-                // calling a select query in the session below.
-                function (callback) {
-                    conn.createStatement(function (err, statement) {
-                        if (err) {
-                            callback(err);
-                        } else {
-                            console.log("Executing query.");
-                            statement.executeQuery(
-                                "SELECT * FROM clients_h_ext LIMIT 10",
-                                function (err, resultset) {
-                                    if (err) {
-                                        console.log(err);
-                                        callback(err);
-                                    } else {
-                                        resultset.toObjArray(function (
-                                            err,
-                                            result
-                                        ) {
-                                            if (result.length > 0) {
-                                                console.log(result);
-                                                res.render("index", {
-                                                    title: "Hello World",
-                                                    query: result,
-                                                });
-                                            }
-                                            callback(null, resultset);
+            conn.createStatement(function (err, statement) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    statement.executeQuery(
+                        "select clients_h_ext.clientid as id, clients_h_ext.age as age, clients_h_ext.sexe as sexe, clients_h_ext.taux as taux, clients_h_ext.situation_familiale as situation_familiale, clients_h_ext.nbr_enfant as nbr_enfant, clients_h_ext.voiture_2 as voiture_2, clients_h_ext.immatriculation as immatriculation, immatriculation_h_ext.marque as marque, immatriculation_h_ext.nom as nom, immatriculation_h_ext.puissance as puissance, immatriculation_h_ext.longueur as longueur, immatriculation_h_ext.nbplaces as nbplaces, immatriculation_h_ext.nbportes as nbportes, immatriculation_h_ext.couleur as couleur, immatriculation_h_ext.occasion as occasion, immatriculation_h_ext.prix as prix from clients_h_ext inner join immatriculation_h_ext on clients_h_ext.immatriculation = immatriculation_h_ext.immatriculation",
+                        function (err, resultset) {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                // Convert the result set to an object array.
+                                resultset.toObjArray(function (err, results) {
+                                    if (results.length > 0) {
+                                        console.log(results.length);
+                                        res.render("index", {
+                                            query: results,
+                                            title: "Hive Query",
                                         });
                                     }
-                                }
-                            );
+                                });
+                            }
                         }
-                    });
-                },
-            ]);
+                    );
+                }
+            });
         }
     });
 });
